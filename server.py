570de97c7706025537ab46ac7a777f0b5a458e39
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-30 23:58:45 +0100

    Add server api boilerplate

---
+++ server.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+
+from flask import Flask, jsonify
+
+from blockchain import Blockchain
+
+app = Flask(__name__)
+
+node_id = str(uuid4()).replace()
+
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return 'Mine a new block!'
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return 'Add a new transaction'
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
